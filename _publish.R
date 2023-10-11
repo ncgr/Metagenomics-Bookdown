f6bd8b425a91de2ce8a7219f079f68b5e902d40b
joannmudge <jm@ncgr.org>
2023-10-11 15:43:40 -0600

    Restructering chapters, remove pdf and epub, made figure smaller.

--- _publish.R
+++ _publish.R
@@ -4,7 +4,7 @@ unlink('_book', recursive = TRUE)
 
 x = readLines('index.Rmd')
 i = 1
-s = paste0('title: "Pangenomics Workshop NCGR (', c('Bootstrap', 'Tufte'), ' Style)"')
+s = paste0('title: "Metagenomics Workshop NCGR (', c('Bootstrap', 'Tufte'), ' Style)"')
 for (fmt in c('html_book', 'tufte_html_book')) {
   unlink('_book', recursive = TRUE)
   file.copy('index.Rmd', '_index.Rmd')
